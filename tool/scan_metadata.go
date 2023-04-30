6e280d97270c90cc9c029e8e28d6c3a119889149
lolgopher <psk5024@gmail.com>
2023-05-01 02:31:45 +0900

    add scan metadata tool

---
+++ tool/scan_metadata.go
@@ -0,0 +1,44 @@
+package tool
+
+import (
+	metadata "github.com/lolgopher/synology-filesync/protocol"
+	"os"
+	"path/filepath"
+)
+
+func GetInitStatus(folderPath string) (map[string]metadata.FileMetadata, error) {
+	return searchMetadata(folderPath, metadata.Init)
+}
+
+func GetNotSentStatus(folderPath string) (map[string]metadata.FileMetadata, error) {
+	return searchMetadata(folderPath, metadata.NotSent)
+}
+
+func GetSentStatus(folderPath string) (map[string]metadata.FileMetadata, error) {
+	return searchMetadata(folderPath, metadata.Sent)
+}
+
+func GetFailedStatus(folderPath string) (map[string]metadata.FileMetadata, error) {
+	return searchMetadata(folderPath, metadata.Failed)
+}
+
+func searchMetadata(folderPath string, status metadata.FileTransferStatus) (map[string]metadata.FileMetadata, error) {
+	var result map[string]metadata.FileMetadata
+
+	err := filepath.Walk(folderPath, func(targetPath string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			data, err := metadata.ReadMetadata(targetPath)
+			if err == nil {
+				for key, value := range data {
+					if value.Status == string(status) {
+						result[key] = value
+					}
+				}
+			}
+		}
+
+		return nil
+	})
+
+	return result, err
+}
