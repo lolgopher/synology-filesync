1ec50f7f927364b6bb4b0304c55fb40f4ff2acca
lolgopher <psk5024@gmail.com>
2023-09-15 17:19:07 +0900

    change format of error

--- protocol/sftp.go
+++ protocol/sftp.go
@@ -30,13 +30,13 @@ func NewSFTPClient(info *ConnectionInfo) (*SFTPClient, error) {
 	addr := fmt.Sprintf("%s:%s", info.IP, info.Port)
 	sshClient, err := ssh.Dial("tcp", addr, sshConfig)
 	if err != nil {
-		return nil, fmt.Errorf("fail to dial: %s", err)
+		return nil, fmt.Errorf("fail to dial: %v", err)
 	}
 
 	// SFTP 클라이언트 생성
 	sftpClient, err := sftp.NewClient(sshClient)
 	if err != nil {
-		return nil, fmt.Errorf("fail to create SFTP client: %s", err)
+		return nil, fmt.Errorf("fail to create SFTP client: %v", err)
 	}
 
 	return &SFTPClient{
@@ -64,7 +64,7 @@ func (sc *SFTPClient) SendFile(localFilePath, remoteFilePath string) (int, error
 	// 파일 열기
 	localFile, err := os.Open(localFilePath)
 	if err != nil {
-		return 0, fmt.Errorf("fail to open local file: %s", err)
+		return 0, fmt.Errorf("fail to open local file: %v", err)
 	}
 	defer func() {
 		if err := localFile.Close(); err != nil {
@@ -74,21 +74,21 @@ func (sc *SFTPClient) SendFile(localFilePath, remoteFilePath string) (int, error
 
 	localFileContent, err := io.ReadAll(localFile)
 	if err != nil {
-		return 0, fmt.Errorf("fail to read local file: %s", err)
+		return 0, fmt.Errorf("fail to read local file: %v", err)
 	}
 
 	// 경로 생성
 	dir := filepath.Dir(remoteFilePath)
 	if _, err := sc.Client.Stat(dir); os.IsNotExist(err) {
 		if err := sc.Client.MkdirAll(dir); err != nil {
-			return 0, fmt.Errorf("fail to create remote dir: %s", err)
+			return 0, fmt.Errorf("fail to create remote dir: %v", err)
 		}
 	}
 
 	// 파일 전송
 	newFile, err := sc.Client.OpenFile(remoteFilePath, os.O_CREATE|os.O_WRONLY|os.O_EXCL)
 	if err != nil {
-		return 0, fmt.Errorf("fail to create remote file: %s", err)
+		return 0, fmt.Errorf("fail to create remote file: %v", err)
 	}
 	defer func() {
 		if err := newFile.Close(); err != nil {
@@ -98,7 +98,7 @@ func (sc *SFTPClient) SendFile(localFilePath, remoteFilePath string) (int, error
 
 	size, err := newFile.Write(localFileContent)
 	if err != nil {
-		return 0, fmt.Errorf("fail to write to remote file: %s", err)
+		return 0, fmt.Errorf("fail to write to remote file: %v", err)
 	}
 
 	return size, nil
